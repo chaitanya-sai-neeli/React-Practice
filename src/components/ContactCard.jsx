29d70ae017050a8408667defa8b9452bf8de2477
chaitanya-sai-neeli <chaitanyasaichaitu123@gmail.com>
2025-04-09 20:21:36 +0530

    Adjusted Contact card UI- Trash can right isde

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -7,15 +7,24 @@ const ContactCard = ({ contact, clickHandler }) => {
   return (
     <div className="item" key={id}>
       <img className="ui avatar image" src={user} alt="user" />
-      <div className="content">
-        <div className="header">{name}</div>
-        <div>{email}</div>
-      </div>
-      <i
-        className="trash alternate outline icon"
+      <div
+        className="content"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <div>
+          <div className="header">{name}</div>
+          <div>{email}</div>
+        </div>
+        <i
+          className="trash alternate outline icon"
           style={{ color: "red", cursor: "pointer" }}
           onClick={() => clickHandler(id)}
-      ></i>
+        ></i>
+      </div>
     </div>
   );
 };

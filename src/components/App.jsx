b6aa031e9607718875698236c0863367d3bb6549
chaitanya-sai-neeli <chaitanyasaichaitu123@gmail.com>
2025-04-09 11:55:37 +0530

    Created and added few components

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import ContactCards from "./ContactCard";
+import ContactList from "./ContactList";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;

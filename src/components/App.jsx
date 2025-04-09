f0874c24e6ea3dd96294664c78ed4ae839f44809
chaitanya-sai-neeli <chaitanyasaichaitu123@gmail.com>
2025-04-09 18:51:54 +0530

    Added contacts in LocalStorage to retain the data even after page refresh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,37 @@
 import Header from "./Header";
 import ContactList from "./ContactList";
 import AddContact from "./AddContact";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   // const contacts = [
   //   { id: 1, name: "John", email: "john@gmail.com" },
   //   { id: 2, name: "David", email: "david@gmail.com" },
   // ];
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+  const isFirstRender = useRef(true);
+
   const addContactsHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact]);
   };
+
+  useEffect(() => {
+    const retrivedContacts = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY)
+    );
+    if (retrivedContacts) setContacts(retrivedContacts);
+  }, []);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className="ui container">
